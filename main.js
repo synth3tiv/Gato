948305893c0b75f658c989d945a66b0048344f35
Samantha <ada_hack479@hackademy.mx>
2022-02-16 15:19:03 -0600

    Añadi boton para reiniciar el juego

--- main.js
+++ main.js
@@ -68,3 +68,28 @@ const verificaVictoria = () => {
     alert("Ganaste!");
   }
 }
+
+const reload = () => {
+  const div1 = document.getElementById(1);
+  const div2 = document.getElementById(2);
+  const div3 = document.getElementById(3);
+  const div4 = document.getElementById(4);
+  const div5 = document.getElementById(5);
+  const div6 = document.getElementById(6);
+  const div7 = document.getElementById(7);
+  const div8 = document.getElementById(8);
+  const div9 = document.getElementById(9);
+
+  div1.innerText = "";
+  div2.innerText = "";
+  div3.innerText = "";
+  div4.innerText = "";
+  div5.innerText = "";
+  div6.innerText = "";
+  div7.innerText = "";
+  div8.innerText = "";
+  div9.innerText = "";
+
+  turno = false;
+  victoria = false;
+}
